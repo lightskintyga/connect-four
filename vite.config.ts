90eeda739652d9fce13734b8a101f04aeae85e45
lightskintyga <daniilivanov7010@gmail.com>
2025-11-05 16:03:16 +0500

    chore: configure TypeScript and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@widgets': path.resolve(__dirname, './src/widgets')
+    },
+  },
 })
